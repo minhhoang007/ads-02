7811f52522bbb650bdddaeceb00d2dc2017fd709
minhhoang <hoangvanminh007@gmail.com>
2021-04-06 17:38:56 +0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,62 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Quảng cáo lào cai",
+    description: "Thiết kế in ấn sản phẩm quảng cáo và các chiến dịch marketing",
+    author: "@minhhoang",
+    person: { name: "john", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `khaob77uuxqh`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: `4VBBSP0s27AQdFxtfkY5l_by8qlysI3ZJv5mQ4eUmKE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import AllRecipes from "../components/AllRecipes"
+import SEO from "../components/SEO"
+export default function Home() {
+  return (
+    <Layout>
+      <SEO title="Home " />
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Nhà Đẹp Lào Cai</h1>
+              <h4>Nơi ý tưởng thành hiện thực</h4>
+            </div>
+          </div>
+        </header>
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
+}
