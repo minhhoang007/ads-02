d07b3f2ba76d8a154241e2e8712862286b0240a0
minhhoang <hoangvanminh007@gmail.com>
2021-04-08 15:51:04 +0700

    remvoe carousel multiple

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,7 @@ import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import AllRecipes from "../components/AllRecipes"
 import SEO from "../components/SEO"
-
-import "react-multi-carousel/lib/styles.css"
 import Carousel2 from "../components/Carousel"
-
-
-
 
 export default function Home() {
   return (
